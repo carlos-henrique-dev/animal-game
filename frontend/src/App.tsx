dec658c4b42b87403834e7ce5f269f70b68352ff
Carlos Henrique <henriqueok20@gmail.com>
2022-11-15 20:15:17 -0400

    corrige async startup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import { ProjectSetup } from './config/project-setup'
-
-const setup = await ProjectSetup()
-
-const router = createBrowserRouter(
-  setup.map(({ route }) => ({
-    path: route?.path,
-    element: route?.element,
-  }))
-)
-
-function App() {
-  return <RouterProvider router={router} />
+function App({ data }: { data: any }) {
+  return <RouterProvider router={data.router} />
 }
 
 export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { createBrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import { ProjectSetup } from './config/project-setup'
+
+async function setup() {
+  const data = await ProjectSetup()
+
+  const router = createBrowserRouter(
+    data.map(({ route }) => ({
+      path: route?.path,
+      element: route?.element,
+    }))
+  )
+
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <App data={{ router }} />
+    </React.StrictMode>
+  )
+}
+
+setup()
